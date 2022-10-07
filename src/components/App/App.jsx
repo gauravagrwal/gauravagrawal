32a3b0fdf04999215765457e83faa78c40c09ae2
Gaurav Agrawal <gauravag.1005@gmail.com>
2022-10-07 20:09:03 +0530

    Created <Resume /> component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,12 @@
 import Header from '../Header/Header';
+import Resume from '../Resume/Resume';
 import './App.css';
 
 export default function App() {
   return (
     <div>
       <Header />
+      <Resume />
     </div>
   );
 }
