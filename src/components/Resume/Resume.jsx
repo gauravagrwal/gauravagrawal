89720f84d31496e1a02c2874aaf745654e04012e
Gaurav Agrawal <gauravag.1005@gmail.com>
2022-10-08 02:18:42 +0530

    Updated <Resume /> component to use lists and keys
    https://reactjs.org/docs/lists-and-keys.html

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,42 +1,62 @@
 import './Resume.scss';
 
+const workExperience = [
+    {
+        title: "Software Engineer/Analyst",
+        company: "Capgemini, Hyderabad, IN",
+        duration: "Oct, 2020 - Present"
+    },
+    {
+        title: "Subject Matter Expert - Computer Science",
+        company: "Chegg, Remote",
+        duration: "Nov, 2019 - Present"
+    }
+];
+
+const educationHistory = [
+    {
+        class: "Bachelor of Technology, Computer Science",
+        school: "RSR Rungta College of Engineering & Technology, Bhilai, Chhattisgarh, IN",
+        duration: "2016 - 2020"
+    },
+    {
+        class: "Senior Secondary, XII",
+        school: "Shri Shankaracharya Vidyalaya, Bhilai, Chhattisgarh, IN",
+        duration: "2016"
+    },
+    {
+        class: "Higher Secondary, X",
+        school: "Ambuja Vidya Peeth, Rawan, Chhattisgarh, IN",
+        duration: "2014"
+    }
+];
+
 export default function Resume() {
     return (
-        <div className='resume'>
+        <div className='row my-resume'>
 
-            <div className='row'>
-                <div className='col-lg-6'>
-                    <h3 className='resume-title'>Professional Experience</h3>
-                    <div className='resume-item'>
-                        <h4>Software Engineer/Analyst</h4>
-                        <p><em>Capgemini, Hyderabad, IN</em></p>
-                        <h5>Oct, 2020 - Present</h5>
+            <div className='col-lg-6'>
+                <h3 className='heading'>Work Experience</h3>
+                {workExperience.map((item, i) => (
+                    <div className='resume-items' key={i}>
+                        <h4>{item.title}</h4>
+                        <p><em>{item.company}</em></p>
+                        <h5>{item.duration}</h5>
                     </div>
-                    <div className='resume-item'>
-                        <h4>Subject Matter Expert - Computer Science</h4>
-                        <p><em>Chegg, Remote</em></p>
-                        <h5>Nov, 2019 - Present</h5>
-                    </div>
-                </div>
-                <div className='col-lg-6'>
-                    <h3 className='resume-title'>Education</h3>
-                    <div className='resume-item'>
-                        <h4>Bachelor of Engineering, Computer Science</h4>
-                        <p> <em>RSR Rungta College of Engineering & Technology, Bhilai, Chhattisgarh, IN</em> </p>
-                        <h5>2016 - 2020</h5>
-                    </div>
-                    <div className='resume-item'>
-                        <h4>Senior Secondary</h4>
-                        <p> <em>Shri Shankaracharya Vidyalaya, Bhilai, Chhattisgarh, IN</em> </p>
-                        <h5>2014 - 2016</h5>
-                    </div>
-                    <div className='resume-item'>
-                        <h4>Higher Secondary</h4>
-                        <p><em>Ambuja Vidya Peeth, Rawan, Chhattisgarh, IN</em></p>
-                        <h5>2012 - 2014</h5>
-                    </div>
-                </div>
+                ))}
             </div>
+
+            <div className='col-lg-6'>
+                <h3 className='heading'>Education</h3>
+                {educationHistory.map((item, i) => (
+                    <div className='resume-items' key={i}>
+                        <h4>{item.class}</h4>
+                        <p><em>{item.school}</em></p>
+                        <h5>{item.duration}</h5>
+                    </div>
+                ))}
+            </div>
+
         </div>
     );
 }
